273eccc0901b3276b9fdb640b53277156c77b220
travisallen24 <travis24@tamu.edu>
2026-03-03 22:16:04 -0600

    lowered the i2c frequency to make the pm sensor more stable

--- microcontroller_code/code.py
+++ microcontroller_code/code.py
@@ -27,7 +27,7 @@ class AirQuality:
         self.sd_logger = logger
 
         # Initialize I2C (and wait until ready)
-        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.i2c = busio.I2C(board.SCL, board.SDA, frequency=100_000)
         while not self.i2c.try_lock():
             pass
         self.i2c.unlock()
