8e7492d6de919cea765d7b8d5e017defedc760b6
travisallen24 <travis24@tamu.edu>
2026-05-27 19:38:47 -0500

    updated set_rtc_time for better ease of use

--- microcontroller_code/set_rtc_datetime.py
+++ microcontroller_code/set_rtc_datetime.py
@@ -8,7 +8,16 @@ i2c = busio.I2C(board.SCL, board.SDA)
 rtc = PCF8523(i2c)
 
 # Set to current time (replace with your actual current time)
+
+year = 2026
+month = 5
+day = 27
+hour = 19
+minute = 38
+second = 0
+
 # Format: (year, month, day, hour, minute, second, weekday, yearday, isdst)
 # Weekday: Monday is 0, Sunday is 6
-rtc.datetime = time.struct_time((2026, 3, 2, 14, 58, 0, 0, -1, -1))
+rtc.datetime = time.struct_time((year, month, day, hour, minute, second, 0, -1, -1))
+
 
